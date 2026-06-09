1a8c5a5c72dde3c3b4f8dfab4e28c3bbf62557f9
DavePaurillo <paurillo.dave@gmail.com>
2026-06-09 20:35:37 +0800

    chore: storybook setup

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from 'eslint-plugin-storybook';
+
 import js from '@eslint/js';
 import globals from 'globals';
 import { defineConfig, globalIgnores } from 'eslint/config';
@@ -68,4 +71,5 @@ export default defineConfig([
     },
   },
   eslintConfigPrettier,
+  ...storybook.configs['flat/recommended'],
 ]);
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,15 +4,49 @@ import tailwindcss from '@tailwindcss/vite';
 import { fileURLToPath } from 'url';
 
 // https://vite.dev/config/
+import path from 'node:path';
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+import { playwright } from '@vitest/browser-playwright';
+const dirname =
+  typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  test: {
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
-  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  test: {
+    projects: [
+      {
+        extends: true,
+        test: {
+          environment: 'jsdom',
+          setupFiles: './src/test/setup.ts',
+        },
+      },
+      {
+        extends: true,
+        plugins: [
+          storybookTest({
+            configDir: path.join(dirname, '.storybook'),
+          }),
+        ],
+        test: {
+          name: 'storybook',
+          browser: {
+            enabled: true,
+            headless: true,
+            provider: playwright({}),
+            instances: [
+              {
+                browser: 'chromium',
+              },
+            ],
+          },
+        },
+      },
+    ],
   },
 });
