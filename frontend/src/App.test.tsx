b4888f5fcdc1fd55d77d36c99d5c59a09cbee462
DavePaurillo <paurillo.dave@gmail.com>
2026-06-09 13:06:23 +0800

    chore: tailwind css setup - add tailwind and prettier - updated settings.json - other files were from pnpm format and pnpm lint:fix

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,11 +1,11 @@
-import { describe, expect, it } from "vitest";
-import { render, screen } from "@testing-library/react"
-import App from "./App";
+import { describe, expect, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-describe("App", () => {
+describe('App', () => {
   // This is just a boilerplate test
   it('displays test on the screen', () => {
-    render(<App />)
-    expect(screen.getByText(/test/i)).toBeInTheDocument()
-  })
-})
+    render(<App />);
+    expect(screen.getByText(/test/i)).toBeInTheDocument();
+  });
+});
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,5 @@
 function App() {
-  return (
-    <p>
-      test
-    </p>
-  )
+  return <p className="text-3xl font-bold text-red-400 underline">test</p>;
 }
 
-export default App
+export default App;
